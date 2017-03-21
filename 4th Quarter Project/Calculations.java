44bb8095d73041bc8f97f496992073686e0ae4b5
SeanFitz0317 <sfitzpatrick210@gmail.com>
2017-03-21 10:31:41 -0400

    Add files via upload

---
+++ 4th Quarter Project/Calculations.java
@@ -0,0 +1,12 @@
+
+public class Calculations {
+	//Flight time (Output)
+	//Angle (Output)
+	//Elevation (User Input)
+	//Wind speed (User Input)
+	//Wind angle (User Input)
+	//Muzzle velocity (User Input)
+	//Range (User Input)
+	//Projectile drop (Output)
+	//Projectile Mass (User Input)
+}
