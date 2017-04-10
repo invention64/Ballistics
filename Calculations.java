0bec7ff37b8d1a5ba8ba22b3c0ed447996602b6b
SeanFitz0317 <sfitzpatrick210@gmail.com>
2017-04-10 10:09:41 -0400

    Add files via upload
    added calculations for wind adjustment

--- Calculations.java
+++ Calculations.java
@@ -6,39 +6,50 @@ public class Calculations {
 	//Flight time(s) (Output)
 	//Elevation(m) (User Input)
 	//Wind speed(km/h) (User Input)
-	//Wind angle N/S/E/W(°) (User Input)
+	//Wind angle N/S/E/W(?) (User Input)
 	//Muzzle velocity(m/s)(Based on User Input Caliber)
 	//Range(m) (User Input)
 	//Variation Left/Right of target (m) (Output)
-	//How far to correct (°) (Output)
-	//Angle N/S/E/W	(°) (Input)
-	public static void main (String[] args)
+	//How far to correct (?) (Output)
+	//Angle N/S/E/W	(?) (Input)
+	public static double[] calculate (double velocity,double range,double elevation, double wspeed, double wangle, double compangle)
 	{
 		@SuppressWarnings("resource")
-		java.util.Scanner input = new java.util.Scanner (System.in);
-		System.out.println("Range (m):");
-		Double a=Double.parseDouble(input.nextLine());
-		System.out.println("Muzzle Velocity (m/s):");
-		Double b=Double.parseDouble(input.nextLine());
-		System.out.println("Elevation (m):");
-		Double c=Double.parseDouble(input.nextLine());	
-		System.out.println("Wind Speed (km/h):");
-		Double d=Double.parseDouble(input.nextLine());
-		System.out.println("Wind Direction (°):");
-		Double e=Double.parseDouble(input.nextLine());
 		//horizontal velocity
-		double v=Math.sqrt((b*b)+2*-9.81*c);
+		double wdiff=0;
+		double windage=0;
+		double v = (Math.pow(velocity,2))-((2.0*9.81*elevation));
+		double v2= Math.sqrt(v);
 		//flight time
-		double t=((v-b)/-9.81);
-		System.out.println("Time to Target: " +t);
+		double t=range/(velocity-(velocity-v2));
 		//change in height
 			//allows for confirmation of value utilized relative to input value
-		double y=(0.5*-9.81*(t*t)+b*t);
+		double y=(0.5*-9.81*(t*t)+velocity*t);
 		//firing angle
-		double cos=(Math.acos((b*t)/a));
+		double cos=(Math.acos(range/(velocity*t)));
 		double ang=(cos*180);
 		double fang=(ang/(Math.PI));
-		double ffang=(90-fang);
-		System.out.println("firing angle" +ffang);	
+		//windage
+		double diff=wangle-compangle;
+		if(diff==90 || compangle-wangle==90) {
+			windage = wspeed/t;
+		}
+		if(diff<90){
+			wdiff=(wangle-compangle);
+		}
+		if(diff>90 && diff<180){
+			wdiff=180-(wangle-compangle);
+		}
+		if(diff>180 && diff<270){
+			wdiff=(wangle-compangle)-180;
+		}
+		if(diff>270 && diff<360) {
+			wdiff=360-(wangle-compangle);
+		}
+		if(diff!=90 && diff!=0){
+			windage=wspeed*Math.sin(wdiff);
+		}
+		double output[] = {t,fang,wdiff,windage};
+		return output;
 	}
 }
